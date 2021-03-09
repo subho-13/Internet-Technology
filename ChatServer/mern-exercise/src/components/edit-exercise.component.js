6bf75d2bf9bfd768aaa9c1ec82f76d26dac00907
subho-13 <souvikdutta6635h@gmail.com>
2021-03-09 23:46:55 +0530

    Updated

---
+++ ChatServer/mern-exercise/src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class EditExercises extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Edit Exercises component!</p>
+            </div>
+        )
+    }
+}
+
+export default EditExercises
---
+++ ChatServer/mern-exercise/src/components/exercises-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Exercises List component!</p>
+            </div>
+        )
+    }
+}
+
+export default ExercisesList
